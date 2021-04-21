cd22e1511fe04869e347a3d79feac21e0d0bfe6d
Cyrine <syrine.benhassine@esprit.tn>
2021-04-21 15:23:13 +0100

    init branch dev

--- src/tunlancer/TunlancerDesktop.java
+++ src/tunlancer/TunlancerDesktop.java
@@ -39,6 +39,8 @@ public class TunlancerDesktop extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        
     }
 
     /**
